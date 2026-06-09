a6e8a879432859ded02695ff03f1840971ebf588
Sharon Moegi <sharamoegi@gmail.com>
2026-06-09 22:08:42 +0300

    Return to original state to create branch

--- lib/book.py
+++ lib/book.py
@@ -1,7 +1,6 @@
-class Book:
-    def __init__(self, title, page_count):
-        self.title = title
-        # Initializing via the setter property ensures validation occurs immediately
-        self.page_count = page_count
+#!/usr/bin/env python3
 
-  
+class Book:
+    pass
+    
+       
